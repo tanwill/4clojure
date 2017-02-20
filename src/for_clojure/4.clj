b8d5260712407a18468b2c795cf8203e727f7c01
Tanise Williams <tanise.williams@icloud.com>
2017-02-19 21:55:34 -0500

    Solution 4

---
+++ src/for_clojure/4.clj
@@ -0,0 +1,10 @@
+;; tanwill's solution to Intro to Lists
+;; https://4clojure.com/problem/4
+
+Lists can be constructed with either a function or a quoted form.
+
+(= (list __) '(:a :b :c))
+
+;; Solution
+
+:a :b :c
