cf2bf53735a775e6e6dda3ccbbe901435e97926f
Tanise Williams <tanise.williams@icloud.com>
2017-02-19 21:54:03 -0500

    Solution 1

---
+++ src/for_clojure/1.clj
@@ -0,0 +1,10 @@
+;; tanwill's solution to Nothing but the Truth
+;; https://4clojure.com/problem/1
+
+This is a clojure form. Enter a value which will make the form evaluate to true. Don't over think it! If you are confused, see the getting started page. Hint: true is equal to true.
+
+(= __ true)
+
+;; Solution
+
+true
