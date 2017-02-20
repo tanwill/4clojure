75646e365feaca718a76d28ed329b59c2d26a72b
Tanise Williams <tanise.williams@icloud.com>
2017-02-19 21:54:56 -0500

    Solution 2

---
+++ src/for_clojure/2.clj
@@ -0,0 +1,10 @@
+;; tanwill's solution to Simple Math
+;; https://4clojure.com/problem/2
+
+If you are not familiar with polish notation, simple arithmetic might seem confusing.
+
+(= (- 10 (* 2 3)) __)
+
+;; Solution
+
+4
