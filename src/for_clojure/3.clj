3868c76273eb054dd6f3b686b51e452d4918961d
Tanise Williams <tanise.williams@icloud.com>
2017-02-19 21:55:20 -0500

    Solution 3

---
+++ src/for_clojure/3.clj
@@ -0,0 +1,11 @@
+;; tanwill's solution to Intro to Strings
+;; https://4clojure.com/problem/3
+
+Clojure strings are Java strings. This means that you can use any of the Java string methods on Clojure strings.
+test not run	
+
+(= __ (.toUpperCase "hello world"))
+
+;; Solution
+
+"HELLO WORLD"
