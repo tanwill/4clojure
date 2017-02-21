664d8ddd634bc75dfaaab35f75f1c472d50ede9a
Tanise Williams <tanise.williams@icloud.com>
2017-02-20 20:10:05 -0500

    Solution 6

---
+++ src/for_clojure/6.clj
@@ -0,0 +1,12 @@
+;; tanwill's solution to Intro to Vectors
+;; https://4clojure.com/problem/6
+
+Vectors can be constructed several ways. You can compare them with lists. 
+
+Note: the brackets [] surrounding the blanks __ are part of the test case.
+
+(= [__] (list :a :b :c) (vec '(:a :b :c)) (vector :a :b :c))
+
+;; Solution
+
+:a :b :c
