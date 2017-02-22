cbf8b5add006a83d2365cfe6e63b3d212066f560
Tanise Williams <tanise.williams@icloud.com>
2017-02-21 22:54:52 -0500

    Solution 9

---
+++ src/for_clojure/9.clj
@@ -0,0 +1,11 @@
+;; tanwill's solution to Sets: conj
+;; https://4clojure.com/problem/9
+
+
+When operating on a set, the conj function returns a new set with one or more keys "added".
+
+(= #{1 2 3 4} (conj #{1 4 3} __))
+
+;; Solution
+
+2
