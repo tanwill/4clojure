8972f2bc1cf74a11e8d8e73632b463a065ce4db3
Tanise Williams <tanise.williams@icloud.com>
2017-02-22 22:34:55 -0500

    Solution 15

---
+++ src/for_clojure/15.clj
@@ -0,0 +1,16 @@
+;; tanwill's solution to Double Down
+;; https://4clojure.com/problem/15
+
+Write a function which doubles a number.
+
+(= (__ 2) 4)
+
+(= (__ 3) 6)
+
+(= (__ 11) 22)
+
+(= (__ 7) 14)
+
+;; Solution
+
+* 2
