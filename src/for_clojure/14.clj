ac85b32bb88e3e8bfee2789af5ca50b7c6dd2b42
Tanise Williams <tanise.williams@icloud.com>
2017-02-23 23:57:00 -0500

    Solution 14 incomplete

---
+++ src/for_clojure/14.clj
@@ -0,0 +1,15 @@
+;; tanwill's solution to Intro to Functions
+;; https://4clojure.com/problem/14
+
+Clojure has many different ways to create functions.
+
+(= __ ((fn add-five [x] (+ x 5)) 3))
+
+(= __ ((fn [x] (+ x 5)) 3))
+
+(= __ (#(+ % 5) 3))
+
+(= __ ((partial + 5) 3))
+
+;; Solution
+
