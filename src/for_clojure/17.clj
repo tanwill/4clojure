1bc32925d156c43e7f8180cd0560c7a29ab05a94
Tanise Williams <tanise.williams@icloud.com>
2017-02-25 23:16:28 -0500

    Solution 17

---
+++ src/for_clojure/17.clj
@@ -0,0 +1,10 @@
+;; tanwill's solution to map
+;; https://4clojure.com/problem/17
+
+The map function takes two arguments: a function (f) and a sequence (s). Map returns a new sequence consisting of the result of applying f to each item of s. Do not confuse the map function with the map data structure.
+
+(= __ (map #(+ % 5) '(1 2 3)))
+
+;; Solution
+
+'(6 7 8)
